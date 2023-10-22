0b98307a47092fc3475f0a0c5181583d541a1a45
lotfi0310 <lotfilouiz@gmail.com>
2023-10-23 00:22:55 +0200

    stage get done

--- src/app/client/stages/stages.module.ts
+++ src/app/client/stages/stages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { StagesRoutingModule } from './stages-routing.module';
 import { StagesComponent } from './stages.component';
+import { ListstagesComponent } from './liststages/liststages.component';
 
 
 @NgModule({
   declarations: [
-    StagesComponent
+    StagesComponent,
+    ListstagesComponent
   ],
   imports: [
     CommonModule,
